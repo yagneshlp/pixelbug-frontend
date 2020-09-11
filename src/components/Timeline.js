dc6be1cbad2f4b2021590e4f9527b9c31eb0023e
Yagnesh L P <yagneshlp@gmail.com>
2020-09-11 13:57:44 +0530

    Timeline added

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -20,52 +20,164 @@ class timeline extends React.Component{
                                 <div className="inner_wrap">
                                     <div className="circle_img"></div>
                                     <div className="porgram_top">
-                                        <span className=" wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">3.00-4.00pm</span>
-                                        <h4 className=" wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">12 Feb 2020</h4>
+                                        <span className=" wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">Club started</span>
+                                        <h4 className=" wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">Sep 12, 2012</h4>
                                     </div>
                                     <div className="thumb wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">
-                                        <img src="img/program_details/1.png" alt=""/>
-                                    </div>
-                                    <h4 className="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">Mr. Zosoldos</h4>
+                                        <img src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/timeline/1.jpg" alt=""/>
+                                    </div>                                    
                                 </div>
                             </div>
                             <div className="single_propram">
                                 <div className="inner_wrap">
                                     <div className="circle_img"></div>
                                     <div className="porgram_top">
-                                        <span className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".3s">3.00-4.00pm</span>
-                                        <h4 className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".4s">12 Feb 2020</h4>
+                                        <span className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".3s">Photography lessons</span>
+                                        <h4 className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".4s">Oct 15, 2012</h4>
                                     </div>
                                     <div className="thumb wow fadeInRight" data-wow-duration="1s" data-wow-delay=".5s">
-                                        <img src="img/program_details/2.png" alt=""/>
-                                    </div>
-                                    <h4 className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".6s">Mr. Zosoldos</h4>
+                                        <img src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/timeline/2.jpg" alt=""/>
+                                    </div>                                    
                                 </div>
                             </div>
                             <div className="single_propram">
                                 <div className="inner_wrap">
                                     <div className="circle_img"></div>
                                     <div className="porgram_top">
-                                        <span className=" wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">3.00-4.00pm</span>
-                                        <h4 className=" wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">12 Feb 2020</h4>
+                                        <span className=" wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">Instagram Started</span>
+                                        <h4 className=" wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">Jan 10, 2016</h4>
                                     </div>
                                     <div className="thumb  wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">
-                                        <img src="img/program_details/3.png" alt=""/>
-                                    </div>
-                                    <h4 className=" wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">Mr. Zosoldos</h4>
+                                        <img src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/timeline/4.jpg" alt=""/>
+                                    </div>                                    
                                 </div>
                             </div>
                             <div className="single_propram">
                                 <div className="inner_wrap">
                                     <div className="circle_img"></div>
                                     <div className="porgram_top">
-                                        <span className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".3s">3.00-4.00pm</span>
-                                        <h4 className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".4s">12 Feb 2020</h4>
+                                        <span className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".3s">1st Aurora- Aurora'16</span>
+                                        <h4 className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".4s">Aug 8,2016</h4>
                                     </div>
                                     <div className="thumb wow fadeInRight" data-wow-duration="1s" data-wow-delay=".5s">
-                                        <img src="img/program_details/4.png" alt=""/>
+                                        <img src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/timeline/5.jpg" alt=""/>
+                                    </div>                                   
+                                </div>
+                            </div>
+                            <div className="single_propram">
+                                <div className="inner_wrap">
+                                    <div className="circle_img"></div>
+                                    <div className="porgram_top">
+                                        <span className=" wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">Off campus events like IIT BHU</span>
+                                        <h4 className=" wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">2017</h4>
                                     </div>
-                                    <h4 className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".6s">Mr. Zosoldos</h4>
+                                    <div className="thumb  wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">
+                                        <img src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/timeline/6.jpg" alt=""/>
+                                    </div>                                   
+                                </div>
+                            </div>
+                            <div className="single_propram">
+                                <div className="inner_wrap">
+                                    <div className="circle_img"></div>
+                                    <div className="porgram_top">
+                                        <span className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".3s">Aurora'17</span>
+                                        <h4 className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".4s">Feb 18, 2017</h4>
+                                    </div>
+                                    <div className="thumb wow fadeInRight" data-wow-duration="1s" data-wow-delay=".5s">
+                                        <img src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/timeline/7.jpg" alt=""/>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            <div className="single_propram">
+                                <div className="inner_wrap">
+                                    <div className="circle_img"></div>
+                                    <div className="porgram_top">
+                                        <span className=" wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">Videography Team was introduced</span>
+                                        <h4 className=" wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">Jan 30, 2018</h4>
+                                    </div>
+                                    <div className="thumb  wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">
+                                        <img src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/timeline/8.png" alt=""/>
+                                    </div>
+                                  
+                                </div>
+                            </div>
+                            <div className="single_propram">
+                                <div className="inner_wrap">
+                                    <div className="circle_img"></div>
+                                    <div className="porgram_top">
+                                        <span className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".3s">Summer challenge'19</span>
+                                        <h4 className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".4s">May 23, 2019</h4>
+                                    </div>
+                                    <div className="thumb wow fadeInRight" data-wow-duration="1s" data-wow-delay=".5s">
+                                        <img src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/timeline/8.jpg" alt=""/>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            <div className="single_propram">
+                                <div className="inner_wrap">
+                                    <div className="circle_img"></div>
+                                    <div className="porgram_top">
+                                        <span className=" wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">Edit my raw</span>
+                                        <h4 className=" wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">Dec 18, 2019</h4>
+                                    </div>
+                                    <div className="thumb  wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">
+                                        <img src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/timeline/9.jpg" alt=""/>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className="single_propram">
+                                <div className="inner_wrap">
+                                    <div className="circle_img"></div>
+                                    <div className="porgram_top">
+                                        <span className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".3s">Recieved first drone</span>
+                                        <h4 className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".4s">Dec 23, 2019</h4>
+                                    </div>
+                                    <div className="thumb wow fadeInRight" data-wow-duration="1s" data-wow-delay=".5s">
+                                        <img src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/timeline/9.png" alt=""/>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            <div className="single_propram">
+                                <div className="inner_wrap">
+                                    <div className="circle_img"></div>
+                                    <div className="porgram_top">
+                                        <span className=" wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">GAM 2020</span>
+                                        <h4 className=" wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">Jan 4, 2020</h4>
+                                    </div>
+                                    <div className="thumb  wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">
+                                        <img src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/timeline/10.jpg" alt=""/>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className="single_propram">
+                                <div className="inner_wrap">
+                                    <div className="circle_img"></div>
+                                    <div className="porgram_top">
+                                        <span className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".3s">Aurora'20</span>
+                                        <h4 className="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".4s">Feb 1, 2020</h4>
+                                    </div>
+                                    <div className="thumb wow fadeInRight" data-wow-duration="1s" data-wow-delay=".5s">
+                                        <img src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/timeline/11.jpg" alt=""/>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            <div className="single_propram">
+                                <div className="inner_wrap">
+                                    <div className="circle_img"></div>
+                                    <div className="porgram_top">
+                                        <span className=" wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">Instagram 3k followers</span>
+                                        <h4 className=" wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">March 25, 2020</h4>
+                                    </div>
+                                    <div className="thumb  wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">
+                                        <img src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/timeline/12.jpg" alt=""/>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
