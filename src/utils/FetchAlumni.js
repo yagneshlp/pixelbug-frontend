722818bb023063d3c7b195a93ec73b8270d0584c
Yagnesh L P <yagneshlp@gmail.com>
2020-09-12 02:47:33 +0530

    alumni added

--- src/utils/FetchAlumni.js
+++ src/utils/FetchAlumni.js
@@ -8,27 +8,38 @@ function FetchAlumni(props){
     },[]);
 
     const [items, setItems] = useState([]);
+    const questions = {
+        q1: "Inspiration behind joining Pixelbug",
+        q2: "What does Pixelbug mean to you? and how was your experience as a member?",
+        q3: "Most memorable moment that Pixelbug has given you",
+        q4: "How has Pixelbug improved you as a person?"
+    }
 
     const fetchAlumni = async () => {
         const data = await fetch(
-            "https://spreadsheets.google.com/feeds/cells/1OlN_6p1u8usdtbo8cIiD2FArgQJA7blfpLN7bEM_xK4/1/public/full?alt=json"
+            "https://spreadsheets.google.com/feeds/cells/1Y96TAPftOl9Annw48Habx6ZobWGENnKtLVVeAc1wBEE/1/public/full?alt=json"
             );
 
         const members = await data.json();
-        console.log(members);
+        //console.log(members);
         var team=[];
-        var offset = { name: 0, batch : 1, desig : 2, profile : 3,	img : 4 };
-        for (var i=1;i<(members.feed.entry.length/6);i++) {
+        
+        var offset = { name: 0, dept : 1, batch:2, desig : 4, img : 6,	q1 : 8, q2: 9, q3: 10, q4:11 };
+        for (var i=1;i<(members.feed.entry.length/12);i++) {            
             team.splice(i,0,memberBuilder(members.feed, i));
         }
 
         function memberBuilder(feed,seed){
             var member =  {
-                "name": feed.entry[(6*seed) + offset.name].content.$t,
-                "batch": feed.entry[(6*seed) + offset.batch].content.$t,
-                "desig": feed.entry[(6*seed) + offset.desig].content.$t,
-                "profile": feed.entry[(6*seed) + offset.profile].content.$t,
-                "img": feed.entry[(6*seed) + offset.img].content.$t,
+                "name": feed.entry[(12*seed) + offset.name].content.$t,
+                "dept": feed.entry[(12*seed) + offset.dept].content.$t,
+                "batch": feed.entry[(12*seed) + offset.batch].content.$t,
+                "desig": feed.entry[(12*seed) + offset.desig].content.$t,
+                "img": feed.entry[(12*seed) + offset.img].content.$t,
+                "q1": feed.entry[(12*seed) + offset.q1].content.$t,
+                "q2": feed.entry[(12*seed) + offset.q2].content.$t,
+                "q3": feed.entry[(12*seed) + offset.q3].content.$t,                
+                "q4": feed.entry[(12*seed) + offset.q4].content.$t
             }        
             //console.log(member);
             return member;
@@ -38,30 +49,29 @@ function FetchAlumni(props){
     };
 
     return(
-
-        <div className="redHeadings">
-            <h2 className="mb-30">Name</h2>
+<div>
+    {
+        items.map( member =>{
+            return <div className="redHeadings">
+            <h2 className="mb-30">{member.name}</h2>
             <div className="row">
                 <div className="col-md-3">
-                    <img src="../img/elements/d.jpg" alt="" className="img-fluid mb-20"/>
-                    <h4>Batch</h4>
-                    <p>Designation</p>
+                    <img src={member.img} alt="" className="img-fluid mb-20"/>
+                    <h4>{member.batch}</h4>
+                    <p>{member.desig}</p>
                 </div>
                 <div className="col-md-9 mt-sm-20">
-                    <p className="redFont">Recently, the US Federal government banned online casinos from operating in America by making
-                        it illegal to
-                        </p>
-                        <p>transfer money to them through any US bank or payment system. As a result of this law, most
-                        of the popular
-                        online casino networks such as Party Gaming and PlayTech left the United States. Overnight,
-                        online casino
-                        players found themselves being chased by the Federal government. But, after a fortnight, the
-                        online casino
-                        industry came up with a solution and new online casinos started taking root. These began to
-                        </p>
+                    <p className="redFont">{questions.q1}</p><p>{member.q1}</p>
+                    <p className="redFont">{questions.q2}</p><p>{member.q2}</p>
+                    <p className="redFont">{questions.q3}</p><p>{member.q3}</p>
+                    <p className="redFont">{questions.q4}</p><p>{member.q4}</p>
                 </div>
             </div>
         </div>
+        })
+    }
+</div>
+        
 
     );
 
