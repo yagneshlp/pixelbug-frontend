7d13b659279283173f6af0fc91692f2417084707
Yagnesh L P <yagneshlp@gmail.com>
2020-09-12 19:01:27 +0530

    added drone pic

--- src/components/Equipments.js
+++ src/components/Equipments.js
@@ -13,14 +13,14 @@ class equipments extends React.Component{
 				<div className="row">
 					<div className="col-md-4">
 						<div className="single-defination">
-                        <img className="mb-20" src="img/program_details/1.png" alt=""/>
+                        <img className="mb-20 equipImage" src="https://pixelbug-website.s3.ap-south-1.amazonaws.com/equipments/drone.jpg" alt=""/>
 							<h3 className="mb-20">Drone</h3>
 							<p>The eagle of cameras, that presents to us the view of God. The DJI Mavic Mini is the perfect creative and compact companion unlocking a striking new angle to fall in love with our college. We thank Scient Lab and RECAL to have sponsored in procuring and unlocking opportunities.</p>
 						</div>
 					</div>
 					<div className="col-md-4">
 						<div className="single-defination">
-                        <img className="mb-20" src="img/program_details/1.png" alt=""/>
+                        <img className="mb-20 equipImage" src="img/program_details/1.png" alt=""/>
 							<h3 className="mb-20">Camera</h3>
 							<p>Standing true to our word that passion is the sole requirement for this club, our camera provides a first touch to our beloved comrades, opening their gateways to a new world through the viewfinder. The Canon 700D is just the perfect tool for that - simple, yet very capable. </p>
 						</div>
